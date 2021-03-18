c9f1f67627ab133815bc431d22faa0e952e1e643
umutlion <umutcayli@outlook.com>
2021-03-18 03:13:16 +0300

    fixing submodules problems.

---
+++ todo-front/src/App.js
@@ -0,0 +1,156 @@
+import React from 'react';
+import './App.css';
+
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+      this.state = {
+        todoList:[],
+        activeItem:{
+          id:null, 
+          title:'',
+          completed:false,
+        },
+        editing:false,
+      }
+      this.fetchTasks = this.fetchTasks.bind(this)
+      this.handleChange = this.handleChange.bind(this)
+      this.handleSubmit = this.handleSubmit.bind(this)
+  };
+
+
+  componentWillMount(){
+    this.fetchTasks()
+  }
+
+  fetchTasks(){
+    console.log('Fetching...')
+
+    fetch('http://127.0.0.1:8000/api/task-list/')
+    .then(response => response.json())
+    .then(data => 
+      this.setState({
+        todoList:data
+      })
+      )
+  }
+
+  handleChange(e){
+    var name = e.target.name
+    var value = e.target.value
+    console.log('Name:', name)
+    console.log('Value:', value)
+
+    this.setState({
+      activeItem:{
+        ...this.state.activeItem,
+        title:value
+      }
+    })
+  }
+
+  handleSubmit(e){
+    e.preventDefault()
+    console.log('ITEM:', this.state.activeItem)
+
+
+    var url = 'http://127.0.0.1:8000/api/task-create/'
+
+
+
+    fetch(url, {
+      method:'POST',
+      headers:{
+        'Content-type':'application/json',
+      },
+      body:JSON.stringify(this.state.activeItem)
+    }).then((response)  => {
+        this.fetchTasks()
+        this.setState({
+           activeItem:{
+          id:null, 
+          title:'',
+          completed:false,
+        }
+        })
+    }).catch(function(error){
+      console.log('ERROR:', error)
+    })
+
+  }
+
+  startEdit(task){
+    this.setState({
+      activeItem:task,
+      editing:true,
+    })
+  }
+
+
+
+
+
+
+
+  render(){
+    var tasks = this.state.todoList
+    var self = this
+    return(
+        <div className="container">
+
+          <div id="task-container">
+              <div  id="form-wrapper">
+                 <form onSubmit={this.handleSubmit}  id="form">
+                    <div className="flex-wrapper">
+                        <div style={{flex: 6}}>
+                            <input onChange={this.handleChange} className="form-control" id="title" value={this.state.activeItem.title} type="text" name="title" placeholder="Add task.." />
+                         </div>
+
+                         <div style={{flex: 1}}>
+                            <input id="submit" className="btn btn-warning" type="submit" name="Add" />
+                          </div>
+                      </div>
+                </form>
+             
+              </div>
+
+              <div  id="list-wrapper">         
+                    {tasks.map(function(task, index){
+                      return(
+                          <div key={index} className="task-wrapper flex-wrapper">
+
+                            <div onClick={() => self.strikeUnstrike(task)} style={{flex:7}}>
+
+                                {task.completed == false ? (
+                                    <span>{task.title}</span>
+
+                                  ) : (
+
+                                    <strike>{task.title}</strike>
+                                  )}
+  
+                            </div>
+
+                            <div style={{flex:1}}>
+                                <button onClick={() => self.startEdit(task)} className="btn btn-sm btn-outline-info">Edit</button>
+                            </div>
+
+                            <div style={{flex:1}}>
+                                <button onClick={() => self.deleteItem(task)} className="btn btn-sm btn-outline-dark delete">-</button>
+                            </div>
+
+                          </div>
+                        )
+                    })}
+              </div>
+          </div>
+          
+        </div>
+      )
+  }
+}
+
+
+
+export default App;
